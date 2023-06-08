907ba83159944eb3f84ba498afb9ff8f84395107
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-06-07 23:54:53 -0300

    Criados os mappers para lidar com a Recipe Entity.

--- app/src/main/java/com/example/flavorquest/data/mappers/RecipeMappers.kt
+++ app/src/main/java/com/example/flavorquest/data/mappers/RecipeMappers.kt
@@ -1,9 +1,9 @@
 package com.example.flavorquest.data.mappers
 
 import com.example.flavorquest.core.getRecipeId
+import com.example.flavorquest.data.local.RecipeEntity
 import com.example.flavorquest.data.remote.model.RecipeDto
 import com.example.flavorquest.domain.model.Recipe
-
 
 fun RecipeDto.toRecipe(): Recipe {
     
@@ -18,5 +18,37 @@ fun RecipeDto.toRecipe(): Recipe {
         diet = diet?: emptyList(),
         source = source ?: "",
         url = url ?: ""
+    )
+}
+
+fun RecipeDto.toRecipeEntity(): RecipeEntity {
+    
+    return RecipeEntity(
+        id = uri?.getRecipeId() ?: "",
+        imageUrl = imageUrl?: "",
+        name = name ?: "",
+        ingredients = ingredients?: emptyList(),
+        cuisineType = cuisineType?: emptyList(),
+        mealType = mealType?: emptyList(),
+        dishType = dishType?: emptyList(),
+        diet = diet?: emptyList(),
+        source = source ?: "",
+        url = url ?: ""
+    )
+}
+
+fun RecipeEntity.toRecipe(): Recipe{
+    
+    return Recipe(
+        id = id,
+        imageUrl = imageUrl,
+        name = name,
+        ingredients = ingredients,
+        cuisineType = cuisineType,
+        mealType = mealType,
+        dishType = dishType,
+        diet = diet,
+        source = source,
+        url = url
     )
 }
